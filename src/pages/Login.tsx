954b792c6919bb1431dd982c946b130ef21eb648
harishs20 <harish.2024b@vitstudent.ac.in>
2026-09-06 12:01:28 +0530

    minor update

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -38,7 +38,7 @@ export const Login: React.FC = () => {
           </div>
         </div>
         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900 tracking-tight">
-          ZEUS Project Constants Pvt Ltd
+          ZEUS Project Consultants Pvt Ltd
         </h2>
         <p className="mt-2 text-center text-sm text-gray-600">
           Historical Data Collection Portal
